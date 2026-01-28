41f93343f6c71e06ce06e2f64e114ac9e17894c8
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:50:45 +0100

    Update auth.interceptor.ts

--- src/app/http/auth.interceptor.ts
+++ src/app/http/auth.interceptor.ts
@@ -1,17 +1,40 @@
 import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../api/auth-service';
+import { environment } from '../../environment/environment';
 
+/**
+ * Captura todas las peticiones HTTP salientes.
+ * Si existe token válido en {@link AuthService},
+ * lo inyecta en las cabeceras.
+ * @param req La petición HTTP saliente.
+ * @param next El siguiente manejador en la cadena de interceptores
+ * @returns Un flujo de evento HTTP con la petición.
+ * @example
+ * Si el token es 'abc', añade la cabecera:
+ * Authorization: Basic abc
+ */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
   const token = auth.getToken();
 
+  //Si no hay token, la petición continua su curso normal.
   if (!token) return next(req);
 
-  // opcional: solo a la API
-  if (!req.url.startsWith('http://localhost:9002')) return next(req);
+  /**
+   * FILTRO DE SEGURIDAD POR URL:
+   * Solo inyectamos credenciales si la petición va dirigida
+   * a nuestro servidor API. Evita enviar contraseñas a
+   * servicios externos.
+   * @important Esta URL debe coincidir con la del
+   * backend en producción.
+   */
+  if (!req.url.startsWith(environment.apiUrl)) return next(req);
 
-  return next(req.clone({
-    setHeaders: { Authorization: `Basic ${token}` },
-  }));
+  // Clon de la petición original y añadida a la cabecera de Auth.
+  return next(
+    req.clone({
+      setHeaders: { Authorization: `Basic ${token}` },
+    }),
+  );
 };
