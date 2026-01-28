170e51439cb439a36f1b4100d0bcb83432d3cc90
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:50:37 +0100

    Update nav-component.ts

--- src/app/components/nav-component/nav-component.ts
+++ src/app/components/nav-component/nav-component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../api/auth-service';
 
+/**
+ * Componente de navegación principal.
+ * Barra de navegación persistente en la parte inferior
+ * de la pantalla.
+ * Usa {@link RouterModule} para gestionar cambio de vistas
+ * sin recargar la página.
+ */
 @Component({
   selector: 'app-nav-component',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav-component.html',
   styleUrl: './nav-component.css',
 })
 export class NavComponent {
+  constructor(private auth: AuthService) {}
 
+  get esPersonal(): boolean {
+    const role = this.auth.getRole();
+    return role === 'BARRA' || role === 'CAMARERO' || role === 'ADMIN';
+  }
 }
