e63c6581fd5d163b21dac535c1e7bf5d444deede
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:50:27 +0100

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./components/nav-component/nav-component";
-import { FooterComponent } from "./components/footer-component/footer-component";
+import { NavComponent } from './components/nav-component/nav-component';
+import { FooterComponent } from './components/footer-component/footer-component';
 import { AuthService } from './api/auth-service';
 
+/**
+ * Componente principal de la aplicación SmartMenu.
+ * Actúa como el contenedor raíz que contiene la navegación
+ * y el área principal de visualización mediante {@link RouterOutlet}.
+ */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('smart-menu-front');
 
-  constructor(private auth: AuthService) {
-  this.auth.setCredentials('empresa@logistica.com', '1234');
-}
+  constructor() {}
 }
