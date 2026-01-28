e5d90199eb3ea25174d1e103a7a7790c0fb5abb5
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:50:24 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,56 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/role.guard';
+import { roleGuard } from './guards/role.guard';
 import { Pagina404 } from './pages/pagina404/pagina404';
 
+/**
+ * Configuración de enrutado principal de SmartMenu.
+ * Define la corresondencia entre las URLs del navegador
+ * y los componentes. Utiliza 'lazy loading' para optimizar
+ * rendimiento.
+ */
 export const routes: Routes = [
+  /** Ruta de acceso inicial y autenticación */
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login').then((m) => m.Login),
   },
 
+  /** Vista principal tras el acceso */
   {
     path: 'inicio',
-    //canActivate: [authGuard],
+    canActivate: [roleGuard],
+    data: { roles: ['CLIENTE', 'BARRA', 'CAMARERO', 'ADMIN'] },
     loadComponent: () => import('./pages/inicio/inicio').then((m) => m.Inicio),
   },
+
+  /** Catálogo de productos (carta digital) */
   {
     path: 'menu',
-    //canActivate: [authGuard],
+    canActivate: [roleGuard],
+    data: { roles: ['CLIENTE', 'BARRA', 'CAMARERO', 'ADMIN'] },
     loadComponent: () => import('./pages/menu/menu').then((m) => m.Menu),
   },
+
+  /** Flujo de creación de pedido y carrito */
   {
     path: 'pedir',
-    //canActivate: [authGuard],
+    canActivate: [roleGuard],
+    data: { roles: ['CLIENTE', 'BARRA', 'CAMARERO', 'ADMIN'] },
     loadComponent: () => import('./pages/pedir/pedir').then((m) => m.Pedir),
   },
-  { path: 'barra', loadComponent: () => import('./pages/barra/barra').then((m) => m.Barra) },
 
+  /** Panel de gestión para personal de restaurante */
+  {
+    path: 'barra',
+    canActivate: [roleGuard],
+    data: { roles: ['BARRA', 'CAMARERO', 'ADMIN'] },
+    loadComponent: () => import('./pages/barra/barra').then((m) => m.Barra),
+  },
+
+  /** Redirección por defecto al entrar en la raíz */
   { path: '', pathMatch: 'full', redirectTo: 'login' },
 
+  /** Manejo de errores y rutas no encontradas */
   { path: '404', component: Pagina404 },
   { path: '**', redirectTo: '404' },
 ];
