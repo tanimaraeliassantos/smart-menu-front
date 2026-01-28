5b2adee274f5b973474a74832f6f74ca7b0620c8
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:51:10 +0100

    Update login.ts

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../api/auth-service';
 
+/** Componente de control de acceso.
+ * Gestiona la captura de las credenciales del usuario
+ * y el flujo de autenticación inicial.
+ * Permite que el {@link roleGuard} gestione los
+ * permisos según el rol.
+ */
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -18,32 +23,44 @@ export class Login {
   loading = false;
   error: string | null = null;
 
+  /**
+   *
+   * @param auth Servicio para la gestión de identidad {@link AuthService}
+   * @param router Servicio de navegación de Angular
+   */
   constructor(
     private auth: AuthService,
-    private http: HttpClient,
-    private router: Router
+    private router: Router,
   ) {}
-
-  async submit() {
+  /**
+   * Procesa el envío del formulario de acceso.
+   */
+  submit() {
     this.error = null;
     this.loading = true;
 
-    try {
-      // Guardamos credenciales (interceptor las meterá como Basic)
-      this.auth.setCredentials(this.email.trim(), this.password);
+    const credentials = {
+      email: this.email.trim(),
+      password: this.password,
+    };
 
-      // “Ping” para validar credenciales (elige un endpoint protegido)
-      // /categoria o /producto si ya lo tienes protegido
-      await this.http.get('http://localhost:9002/producto').toPromise();
-
-      // OK -> entra
-      this.router.navigateByUrl('/inicio');
-    } catch (e: any) {
-      // si falla, limpiamos token para que no se quede guardado
-      this.auth.clear();
-      this.error = 'Credenciales inválidas o tablet no autorizada';
-    } finally {
-      this.loading = false;
-    }
+    /**
+     * Inicia la suscripción al flujo de autenticación.
+     */
+    this.auth.login(credentials).subscribe({
+      next: (response) => {
+        console.log('Usuario autenticado', response.user.nombre);
+        this.loading = false;
+        this.router.navigateByUrl('/inicio');
+      },
+      error: (err) => {
+        this.loading = false;
+        this.error = 'Credenciales inválidas';
+        this.auth.logout();
+      },
+      complete: () => {
+        this.loading = false;
+      },
+    });
   }
 }
