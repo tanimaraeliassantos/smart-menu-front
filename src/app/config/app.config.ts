8762ec90aedbafc49b3ab8cb79d268fb37ef610f
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:50:38 +0100

    Update app.config.ts

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -5,9 +5,15 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from '../app.routes';
 import { authInterceptor } from '../http/auth.interceptor';
 
+/**
+ * Configuración global de la aplicación.
+ * Se definen los proveedores principales que alimentan
+ * SmartMenu.
+ * 1. {@link routes} Activa el sistema de navegación
+ * 2. HttpClient: Habilita las peticiones al servidor
+ * 3. {@link authInterceptor} Asegura que cada petición
+ * lleve el token de seguridad.
+ */
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
 };
