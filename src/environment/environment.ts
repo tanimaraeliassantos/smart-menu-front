c180034792a7bcaf5fb1014080a01d0227bca337
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:48:50 +0100

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,12 @@
+/**
+ * Configuración de Entorno
+ * Centraliza las variables que cambian según es despliegue.
+ * La propiedad {@link apiUrl} es el punto de referencia para:
+ * 1. El {@link ApiClient} al construir las rutas de los
+ * endpoints.
+ * 2. El {@link authInterceptor} para validar a qué dominios
+ * enviamos credenciales.
+ */
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:9002',
