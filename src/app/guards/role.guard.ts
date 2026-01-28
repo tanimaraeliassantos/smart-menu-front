227110eb9abbfb93eb392d96aa416c0376b23708
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-28 23:50:43 +0100

    Update role.guard.ts

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -2,12 +2,35 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../api/auth-service';
 
-export const authGuard: CanActivateFn = () => {
+/**
+ * Guardián de acceso baso en Roles.
+ * Verifica autenticación (Si está logueado)
+ * Verifica autorización (Si tiene el rol adecuado)
+ */
+export const roleGuard: CanActivateFn = (route) => {
   const auth = inject(AuthService);
   const router = inject(Router);
+  //Si no hay credenciales, al login de cabeza
+  if (!auth.hasCredentials()) {
+    router.navigateByUrl('/login');
+    return false;
+  }
 
-  if (auth.hasCredentials()) return true;
+  /** Obtener roles permitidos definidos en las rutas.
+   * Si la ruta no define roles, se permite el paso.
+   */
+  const rolesPermitidos = route.data['roles'] as string[];
+  if (!rolesPermitidos) return true;
 
-  router.navigateByUrl('/login');
+  /** Comprobado el rol del usuario actual */
+  const userRole = auth.getRole();
+
+  if (userRole && rolesPermitidos.includes(userRole)) {
+    return true;
+  }
+
+  /** Si tiene sesión pero no el rol necesario, se va a inicio */
+  console.warn(`Acceso denegado para el rol: ${userRole}`);
+  router.navigateByUrl('/inicio');
   return false;
 };
