cd484fdb1e39d31a8fc714ca7b37232e581f84ca
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-03-02 20:54:07 +0100

    Update auth.interceptor.ts

--- src/app/http/auth.interceptor.ts
+++ src/app/http/auth.interceptor.ts
@@ -5,12 +5,15 @@ import { AuthService } from '../api/auth-service';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
   const token = auth.getToken();
+  console.log('url: ', req.url);
+  console.log('hay token?', token ? 'SI' : 'NO');
+  if (token) console.log('Token', token?.substring(0, 15) + '...');
 
   if (!token) return next(req);
 
   return next(
     req.clone({
       setHeaders: { Authorization: `Bearer ${token}` },
-    })
+    }),
   );
 };
